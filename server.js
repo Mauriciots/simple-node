0da7e0d0d01e51d66c97dd0d892dab0b3e1efd26
Maurício Tomaz <mauricio@MacBook-Pro-de-MacBook.local>
2021-06-08 19:11:22 -0300

    Print out env variable NAME + adjust travis config

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const personName = process.env.NAME
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`${personName} rocks!`);
     await sleep(5000);
   }
 }
